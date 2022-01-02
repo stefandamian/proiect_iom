b0e9cf5058402551f133b9b9a86b1aff3f45f08e
Enrico Garaiman <enrico7899@yahoo.com>
2022-01-02 18:13:37 +0200

    Testare

--- testare/test/vehicle_count.py
+++ testare/test/vehicle_count.py
@@ -16,8 +16,8 @@ cap = cv2.VideoCapture('../video-compress.mp4')
 input_size = 320
 
 # Detection confidence threshold
-confThreshold =0.2
-nmsThreshold= 0.2
+confThreshold = 0.2
+nmsThreshold = 0.2
 
 font_color = (0, 0, 255)
 font_size = 0.5
@@ -27,7 +27,6 @@ font_thickness = 2
 middle_line_position = 225
 up_line_position = middle_line_position - 15
 down_line_position = middle_line_position + 15
-
 
 # Store Coco Names in a list
 classesFile = "coco.names"
@@ -59,11 +58,12 @@ colors = np.random.randint(0, 255, size=(len(classNames), 3), dtype='uint8')
 
 # Function for finding the center of a rectangle
 def find_center(x, y, w, h):
-    x1=int(w/2)
-    y1=int(h/2)
-    cx = x+x1
-    cy=y+y1
+    x1 = int(w / 2)
+    y1 = int(h / 2)
+    cx = x + x1
+    cy = y + y1
     return cx, cy
+
 
 # List for store vehicle count information
 temp_up_list = []
@@ -71,9 +71,9 @@ temp_down_list = []
 up_list = [0, 0, 0, 0]
 down_list = [0, 0, 0, 0]
 
+
 # Function for count vehicle
 def count_vehicle(box_id, img):
-
     x, y, w, h, id, index = box_id
 
     # Find the center of the rectangle for detection
@@ -93,7 +93,7 @@ def count_vehicle(box_id, img):
     elif iy < up_line_position:
         if id in temp_down_list:
             temp_down_list.remove(id)
-            up_list[index] = up_list[index]+1
+            up_list[index] = up_list[index] + 1
 
     elif iy > down_line_position:
         if id in temp_up_list:
@@ -104,13 +104,14 @@ def count_vehicle(box_id, img):
     cv2.circle(img, center, 2, (0, 0, 255), -1)  # end here
     # print(up_list, down_list)
 
+
 # Function for finding the detected objects from the network output
-def postProcess(outputs,img):
+def postProcess(outputs, img):
     global detected_classNames
     height, width = img.shape[:2]
     boxes = []
     classIds = []
-    confidence_scores = []
+    confidenceScores = []
     detection = []
     for output in outputs:
         for det in output:
@@ -119,15 +120,14 @@ def postProcess(outputs,img):
             confidence = scores[classId]
             if classId in required_class_index:
                 if confidence > confThreshold:
-                    # print(classId)
-                    w,h = int(det[2]*width) , int(det[3]*height)
-                    x,y = int((det[0]*width)-w/2) , int((det[1]*height)-h/2)
-                    boxes.append([x,y,w,h])
+                    w, h = int(det[2] * width), int(det[3] * height)
+                    x, y = int((det[0] * width) - w / 2), int((det[1] * height) - h / 2)
+                    boxes.append([x, y, w, h])
                     classIds.append(classId)
-                    confidence_scores.append(float(confidence))
+                    confidenceScores.append(float(confidence))
 
     # Apply Non-Max Suppression
-    indices = cv2.dnn.NMSBoxes(boxes, confidence_scores, confThreshold, nmsThreshold)
+    indices = cv2.dnn.NMSBoxes(boxes, confidenceScores, confThreshold, nmsThreshold)
     # print(classIds)
     for i in indices.flatten():
         x, y, w, h = boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]
@@ -137,8 +137,8 @@ def postProcess(outputs,img):
         name = classNames[classIds[i]]
         detected_classNames.append(name)
         # Draw classname and confidence score
-        cv2.putText(img,f'{name.upper()} {int(confidence_scores[i]*100)}%',
-                  (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+        cv2.putText(img, f'{name.upper()} {int(confidenceScores[i] * 100)}%',
+                    (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
 
         # Draw bounding rectangle
         cv2.rectangle(img, (x, y), (x + w, y + h), color, 1)
@@ -153,7 +153,7 @@ def postProcess(outputs,img):
 def realTime():
     while True:
         success, img = cap.read()
-        img = cv2.resize(img,(0,0),None,0.5,0.5)
+        img = cv2.resize(img, (0, 0), None, 0.5, 0.5)
         ih, iw, channels = img.shape
         blob = cv2.dnn.blobFromImage(img, 1 / 255, (input_size, input_size), [0, 0, 0], 1, crop=False)
 
@@ -165,10 +165,9 @@ def realTime():
         outputs = net.forward(outputNames)
 
         # Find the objects from the network output
-        postProcess(outputs,img)
+        postProcess(outputs, img)
 
         # Draw the crossing lines
-
         cv2.line(img, (0, middle_line_position), (iw, middle_line_position), (255, 0, 255), 2)
         cv2.line(img, (0, up_line_position), (iw, up_line_position), (0, 0, 255), 2)
         cv2.line(img, (0, down_line_position), (iw, down_line_position), (0, 0, 255), 2)
@@ -176,10 +175,14 @@ def realTime():
         # Draw counting texts in the frame
         cv2.putText(img, "Up", (110, 20), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
         cv2.putText(img, "Down", (160, 20), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Car:        "+str(up_list[0])+"     "+ str(down_list[0]), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Motorbike:  "+str(up_list[1])+"     "+ str(down_list[1]), (20, 60), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Bus:        "+str(up_list[2])+"     "+ str(down_list[2]), (20, 80), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-        cv2.putText(img, "Truck:      "+str(up_list[3])+"     "+ str(down_list[3]), (20, 100), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
+        cv2.putText(img, "Car:        " + str(up_list[0]) + "     " + str(down_list[0]), (20, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
+        cv2.putText(img, "Motorbike:  " + str(up_list[1]) + "     " + str(down_list[1]), (20, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
+        cv2.putText(img, "Bus:        " + str(up_list[2]) + "     " + str(down_list[2]), (20, 80),
+                    cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
+        cv2.putText(img, "Truck:      " + str(up_list[3]) + "     " + str(down_list[3]), (20, 100),
+                    cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
 
         # Show the frames
         cv2.imshow('Output', img)
@@ -204,6 +207,8 @@ def realTime():
 
 
 image_file = 'dih_intersection2.jpg'
+
+
 def from_static_image(image):
     img = cv2.imread(image)
 
@@ -217,17 +222,20 @@ def from_static_image(image):
     outputs = net.forward(outputNames)
 
     # Find the objects from the network output
-    postProcess(outputs,img)
+    postProcess(outputs, img)
 
     # count the frequency of detected classes
     frequency = collections.Counter(detected_classNames)
     print(frequency)
     # Draw counting texts in the frame
-    cv2.putText(img, "Car:        "+str(frequency['car']), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-    cv2.putText(img, "Motorbike:  "+str(frequency['motorbike']), (20, 60), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-    cv2.putText(img, "Bus:        "+str(frequency['bus']), (20, 80), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-    cv2.putText(img, "Truck:      "+str(frequency['truck']), (20, 100), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color, font_thickness)
-
+    cv2.putText(img, "Car:        " + str(frequency['car']), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color,
+                font_thickness)
+    cv2.putText(img, "Motorbike:  " + str(frequency['motorbike']), (20, 60), cv2.FONT_HERSHEY_SIMPLEX, font_size,
+                font_color, font_thickness)
+    cv2.putText(img, "Bus:        " + str(frequency['bus']), (20, 80), cv2.FONT_HERSHEY_SIMPLEX, font_size, font_color,
+                font_thickness)
+    cv2.putText(img, "Truck:      " + str(frequency['truck']), (20, 100), cv2.FONT_HERSHEY_SIMPLEX, font_size,
+                font_color, font_thickness)
 
     cv2.imshow("image", img)
 
